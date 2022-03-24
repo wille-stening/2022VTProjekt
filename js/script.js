18dd38c1a2096cc9cca85def427b1157b6040976
wille-stening <94617286+wille-stening@users.noreply.github.com>
2022-03-24 13:47:08 +0100

    jobbat idag med att fixa min meny och att göra i ordning en ruta för min andra delsida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // Denna fil skall innehålla JavaScript (JS)
+function toggleMeny() {
+ 
+document.querySelector('.navlinks').classList.toggle('navActive')
+document.querySelector('.burger').classList.toggle('toggle')
+
+}
+
+document.querySelector('.burger').addEventListener('click', toggleMeny)
+
+
+
+
+
+
+
+
+
+
